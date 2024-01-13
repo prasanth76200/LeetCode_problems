3c595fdb60a8401878215c2459a262bb584c4355
Prasanth.k <139969338+prasanth76200@users.noreply.github.com>
2024-01-13 17:11:37 +0530

    Update 0287-find-the-duplicate-number.java

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,9 +2,7 @@ class Solution {
     public int findDuplicate(int[] nums) {
         int i=0;
         
-        while(i<nums.length){
-            
-            
+        while(i<nums.length){  
             if(nums[i]!=i+1){
             int correct=nums[i]-1;
             if(nums[i]!=nums[correct]){
