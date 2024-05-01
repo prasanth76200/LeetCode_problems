13f5553878d8cf7079a63bfbf0cfb931eac6538e
Prasanth.k <139969338+prasanth76200@users.noreply.github.com>
2024-05-01 13:33:06 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (71.99%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,19 +13,45 @@
  *     }
  * }
  */
-class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-         recur(root,res);
-        return res;
-    }
-    public void recur(TreeNode root,List<Integer> res){
-        if(root==null){
-            return;
-        }
-        recur(root.left,res);
-        res.add(root.val);
-        recur(root.right,res);
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//          recur(root,res);
+//         return res;
+//     }
+//     public void recur(TreeNode root,List<Integer> res){
+//         if(root==null){
+//             return;
+//         }
+//         recur(root.left,res);
+//         res.add(root.val);
+//         recur(root.right,res);
         
-    }
+//     }
+// }
+
+
+class Solution{
+     public List<Integer> inorderTraversal(TreeNode root){
+       List<Integer> res = new ArrayList<>();
+         Stack<TreeNode> stack = new Stack<>();
+         TreeNode curr = root;
+         while(curr!=null || !stack.isEmpty()){
+             
+             if(curr!=null){
+                 stack.push(curr);
+                 curr = curr.left;
+             }
+             else{
+               curr = stack.pop();
+                 res.add(curr.val);
+                 curr = curr.right;
+             }
+         }
+         
+         return res;
+         
+         
+     }
+  
 }
