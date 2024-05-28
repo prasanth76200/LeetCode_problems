f37184c1e12d4888c0bf49f22098b56c7fd9aec0
Prasanth.k <139969338+prasanth76200@users.noreply.github.com>
2024-05-28 19:46:04 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (66.93%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,27 +1,27 @@
 class Solution {
-
     public String reversePrefix(String word, char ch) {
         int find = -1;
         char[] arr = word.toCharArray();
         for (int i = 0; i < word.length(); i++) {
             if (arr[i] == ch) {
                 find = i;
-                break;
+                break;  // Exit the loop once the first occurrence is found
             }
         }
+        
         if (find == -1) {
-            return word;
+            return word;  // If ch is not found, return the original word
         } else {
             int i = 0;
             int j = find;
-            while (i <= j) {
+            while (i < j) {
                 swap(arr, i, j);
                 i++;
                 j--;
             }
         }
         
-        return new String(arr);
+        return new String(arr);  // Convert the char array back to a String
     }
 
     void swap(char arr[], int i, int j) {
