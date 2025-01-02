a0433240edae495499d80beea25314ea4e90eb8d
Prasanth.k <139969338+prasanth76200@users.noreply.github.com>
2025-01-02 21:05:57 +0530

    Time: 1 ms, Memory: 45.8 MB - LeetHub

---
+++ 3448-407-1929-concatenation-of-array/3448-407-1929-concatenation-of-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+            
+        int[] arr = new int[n*2];
+        
+        for(int i=0;i<n;i++){
+            arr[i]=nums[i];
+            arr[i+n] = nums[i];
+            
+        }
+        
+        
+        return arr;
+    }
+}
